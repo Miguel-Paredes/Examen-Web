6e6b3e465c82380934964f694b4c9cf6cd1cd539
Miguel-Paredes <os.miguelparedes@gmail.com>
2023-12-12 15:17:00 -0500

    update

--- src/controllers/index.controllers.js
+++ src/controllers/index.controllers.js
@@ -1,13 +1,15 @@
 
+// Primera funcion para renderizar el index
 const renderIndex = (req,res)=>{
     res.render('index')
 }
 
+// Primera funcion para renderizar el login
 const renderAbout = (req,res)=>{
     res.render('login')
 }
 
-
+// Exportar dos funciones
 module.exports ={
     renderIndex, 
     renderAbout
