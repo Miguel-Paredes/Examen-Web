600dc6389c643304d453cec3a31c84fd39e58091
Miguel-Paredes <os.miguelparedes@gmail.com>
2023-12-18 15:19:30 -0500

    update

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,11 +1,30 @@
+const User = require('../models/User')
+
 // Mostrar el formulario de registro
 const renderRegisterForm =(req,res)=>{
     res.render('user/registerForm')
 }
 
 // Capturar los datos del formulario y almacenar en la BDD
-const registerNewUser =(req,res)=>{
-    res.send('register new user')
+const registerNewUser = async(req,res)=>{
+    // Capturar los datos del body
+    const{name,email,password,confirmpassword} = req.body
+    // Validar todos los campos
+    if (Object.values(req.body).includes("")) return res.send("Lo sentimos, debes llenar todos los campos")
+    // Validar el password
+    if(password != confirmpassword) return res.send("Lo sentimos, los passwords no coinciden")
+    // Validar si el usuario ya esta registrado
+    const userBDD = await User.findOne({email})
+    // Si el usuario ya esta registrado 
+    if(userBDD) return res.send("Lo sentimos, el email ya se encuentra registrado")
+    // Crear una instancia del usuario
+    const newUser = await new User({name,email,password,confirmpassword})
+    // Encriptar el password
+    newUser.password = await newUser.encrypPassword(password)
+    // Guardar en BDD
+    newUser.save()
+    // Redirecionamiento
+    res.redirect('/user/login')
 }
 
 
