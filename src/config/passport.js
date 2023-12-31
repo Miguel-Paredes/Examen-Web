2888b4e2e32b42266dddeb335ce63557915da3cb
AzureAD\10800 <os.miguelparedes@gmail.com>
2023-12-30 20:03:41 -0500

    update

--- src/config/passport.js
+++ src/config/passport.js
@@ -2,6 +2,8 @@
 const passport = require('passport')
 // Importar el modelo User
 const User = require('../models/User')
+// Importar node-notifier
+const notifier = require('node-notifier');
 // Establecer la estrategia
 const LocalStrategy = require('passport-local').Strategy
 
@@ -15,17 +17,29 @@ passport.use(new LocalStrategy({
     // buscar el usuario en base al email
     const userBDD = await User.findOne({email})
     // Verificar si existe el usuario
-    if(!userBDD) return done("Lo sentimos, el email no se encuentra registrado",false,)
+    if(!userBDD) return done(showAlert(errorMessage),false,)
     // Desincriptar el passowrd
     const passwordUser = await userBDD.matchPassword(password)
-    if(!passwordUser) return done("Lo sentimos, los passwords no coinciden",false)
+    if(!passwordUser) return done(showAlert(errorMessage2),false)
     // Validar si el usuairo puede iniciar sesion si y solo si ha confirmado su cuenta de correo electronico
-    if(userBDD.confirmEmail===false) return done("Lo sentimos, debe verificar la cuenta en su correo electrónico",false)
+    if(userBDD.confirmEmail===false) return done(showAlert(errorMessage3),false)
     // Retornar el usuario de BDD
     return done(null,userBDD)
 }))
 
 
+
+function showAlert(message) {
+  notifier.notify({
+    title: '¡Lo sentimos!',
+    message: message,
+    sound: true
+  });
+}
+
+const errorMessage = 'El email no se encuentra registrado.';
+const errorMessage2 = 'Los passwords no coinciden.';
+const errorMessage3 = 'Debe verificar la cuenta en su correo electrónico.';
 
 passport.serializeUser((user,done)=>{
     done(null,user.id)
